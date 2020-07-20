5daae4b45c56593947b58acec8ff592211ae6bfb
Harika1995 <nagaharika.n@gmail.com>
2020-07-20 16:36:29 -0500

    Add code in test.py

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
 print "This is a  new python file created from the new branch called devbranch"
 
+int i=10
+if i <= 5:
+    print "a is less than 5"
+else:
+    print "a is greater than 5"
+
+print "End of the code"
